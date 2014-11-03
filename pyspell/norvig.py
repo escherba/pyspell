fc0dd4cd8bd69a2ea1114bce5020605c6503d5d9
Diane Napolitano <dnapolitano@ets.org>
2014-11-03 15:43:17 -0500

    Now committing Norvig's code in class form.

--- pyspell/norvig.py
+++ pyspell/norvig.py
@@ -1,37 +1,53 @@
-import re, collections
+import re
+from collections import defaultdict
 
 
-NWORDS = train(words(file('big.txt').read()))
+class BasicSpellCorrector(object):
+    '''
+    http://norvig.com/spell-correct.html
+    '''
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-
-def words(text): return re.findall('[a-z]+', text.lower()) 
-
-
-def train(features):
-    model = collections.defaultdict(lambda: 1)
-    for f in features:
-        model[f] += 1
-    return model
+    def __init__(self, words_data_file):
+        with open(words_data_file, 'rU') as fh:
+            self.nwords = self.train(self.words(fh.read()))
+        
+        self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 
-def edits1(word):
-   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-   deletes    = [a + b[1:] for a, b in splits if b]
-   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
-   inserts    = [a + c + b     for a, b in splits for c in alphabet]
-   return set(deletes + transposes + replaces + inserts)
+    def words(self, text):
+        return re.findall('[a-z]+', text.lower()) 
 
 
-def known_edits2(word):
-    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+    def train(self, features):
+        model = defaultdict(lambda: 1)
+        for f in features:
+            model[f] += 1
+        return model
 
 
-def known(words): return set(w for w in words if w in NWORDS)
+    def edits1(self, word):
+        splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes    = [a + b[1:] for a, b in splits if b]
+        transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+        replaces   = [a + c + b[1:] for a, b in splits for c in self.alphabet if b]
+        inserts    = [a + c + b     for a, b in splits for c in self.alphabet]
+        return set(deletes + transposes + replaces + inserts)
 
 
-def correct(word):
-    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
-    return max(candidates, key=NWORDS.get)
+    def known_edits2(self, word):
+        return set(e2 for e1 in self.edits1(word) for e2 in self.edits1(e1) if e2 in self.nwords)
+
+
+    def known(self, words):
+        return set(w for w in words if w in self.nwords)
+
+
+    def correct(self, word):
+        candidates = self.known([word]) or self.known(self.edits1(word)) or self.known_edits2(word) or [word]
+        return max(candidates, key=self.nwords.get)
+
+
+if __name__ == "__main__":
+    corrector = BasicSpellCorrector("../data/big.txt")
+    print corrector.correct("halp")
+    print corrector.correct("mom")
