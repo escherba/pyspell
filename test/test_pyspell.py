039894acebc2467645917cfa66930deb0190220c
Diane Napolitano <dnapolitano@ets.org>
2014-11-06 10:10:00 -0500

    Current P/R on development set: 77.37/63.38.  Lots of false negatives (1677); focus needs to be on false positives (849).

--- test/test_pyspell.py
+++ test/test_pyspell.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, division
+
 import sys
 
 import pandas
@@ -6,13 +8,26 @@ from pyspell import BasicSpellCorrector
 
 
 def evaluate(df, data_path):
-    true_positives = 0 # error was corrected to the expected correction
-    false_negatives = 0 # error was corrected to something else
-    false_positives = 0 # error was corrected to itself
-
     pysp = BasicSpellCorrector(data_path)
     df["correction from pyspell"] = df["error"].apply(lambda x : pysp.correct(x))
-    print df
+
+    # error was corrected to the expected correction
+    true_positives = df[df["correct form"] == df["correction from pyspell"]]
+    # error was corrected to something else
+    false_negatives = df[df["correct form"] != df["correction from pyspell"]]
+    false_negatives = false_negatives[false_negatives["correction from pyspell"] != false_negatives["error"]]
+    # error was corrected to itself
+    false_positives = df[df["correction from pyspell"] == df["error"]]
+
+    print("true positives = " + str(len(true_positives)))
+    print("false negatives = " + str(len(false_negatives)))
+    print("false positives = " + str(len(false_positives)))
+
+    precision = len(true_positives) / (len(true_positives) + len(false_positives))
+    recall = len(true_positives) / (len(true_positives) + len(false_negatives))
+
+    print("precision = " + str(precision * 100))
+    print("recall = " + str(recall * 100))
 
 
 if __name__ == "__main__":
